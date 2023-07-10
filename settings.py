a39f320db796a3add46635709e778e636583e182
unknown <you@example.com>
2023-07-10 23:25:02 +0200

    weapon

--- settings.py
+++ settings.py
@@ -3,3 +3,11 @@ WIDTH    = 1280
 HEIGTH   = 720
 FPS      = 60
 TILESIZE = 64
+
+# weapons 
+weapon_data = {
+	'sword': {'cooldown': 100, 'damage': 15,'graphic':'magic-kingdom/graphics/weapons/sword/full.png'},
+	'lance': {'cooldown': 400, 'damage': 30,'graphic':'magic-kingdom/graphics/weapons/lance/full.png'},
+	'axe': {'cooldown': 300, 'damage': 20, 'graphic':'magic-kingdom/graphics/weapons/axe/full.png'},
+	'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'magic-kingdom/graphics/weapons/rapier/full.png'},
+	'sai':{'cooldown': 80, 'damage': 10, 'graphic':'magic-kingdom/graphics/weapons/sai/full.png'}}
